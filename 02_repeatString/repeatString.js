5a42eb520605d55086339c013cefcf56dd545825
Dante <MrTheAziz@hotmail.com>
2022-11-09 15:18:28 -0500

    Complete second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    let repeat = '';
+    if(times < 0){
+        return 'ERROR';
+    }
 
+    for(let i=1; i<= times; i++) {
+        repeat += word;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
