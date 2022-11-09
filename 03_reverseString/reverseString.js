c4958fde24a39d0dfd708d8bf6da382b668c8469
Dante <MrTheAziz@hotmail.com>
2022-11-09 15:19:14 -0500

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let rWord = '';
+    for(let i=word.length-1; i >= 0; i--) {
+        rWord += word[i];
+    }
+    return rWord;
 };
 
 // Do not edit below this line
