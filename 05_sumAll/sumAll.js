4ea826b396dcc7d39600f03fbcda4b71e22ee21f
Dante <MrTheAziz@hotmail.com>
2022-11-10 11:40:42 -0500

    Complete exercise number 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let hasNegativesOrNonNumbers = args.some(element => {
+        return element < 0 || typeof element != 'number'; 
+    })
 
+    if(hasNegativesOrNonNumbers) {
+        return 'ERROR'; 
+    }
+
+    let sum = 0;
+    for(let i=Math.min(...args); i<=Math.max(...args); i++){
+        sum += i; 
+    }
+    return sum;    
 };
 
 // Do not edit below this line
