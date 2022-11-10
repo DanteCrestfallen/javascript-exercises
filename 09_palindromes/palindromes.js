db3f55ee3514918f804628a181405adfb20fd1bf
Dante <MrTheAziz@hotmail.com>
2022-11-10 15:11:35 -0500

    Complete exercise number 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let removePunctuationAndSpace = (element => {
+        return element.replace(/[^A-Za-z0-9]/g, '');
+    });
 
+    word = removePunctuationAndSpace(word);
+
+    let reverseWord = '';
+    for(let i=word.length-1; i >= 0; i--) {
+        reverseWord += word[i];
+    }
+    return word.toLowerCase() === reverseWord.toLowerCase();
 };
 
 // Do not edit below this line
