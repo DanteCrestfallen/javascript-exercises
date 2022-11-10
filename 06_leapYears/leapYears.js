ae46e84f77145154a40fd68fb0627f1d773b3ca3
Dante <MrTheAziz@hotmail.com>
2022-11-10 11:40:50 -0500

    Complete exercise number 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isDivisibleByFour = (element => { 
+        return element % 4 == 0; 
+    });
 
+    let isDivisibleByOneHundred = (element => { 
+        return element % 100 == 0; 
+    });
+
+    let isDivisibleByFourHundred = (element => { 
+        return element % 400 == 0; 
+    });
+
+    return (isDivisibleByFour(year) && !isDivisibleByOneHundred(year) || isDivisibleByFourHundred(year));
 };
 
 // Do not edit below this line
