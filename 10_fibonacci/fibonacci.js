82ea672935073c2d9fea307382d497bf65fc0656
Dante <MrTheAziz@hotmail.com>
2022-11-10 15:46:59 -0500

    Complete exercise number 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) {
+        return 'OOPS';
+    }
+    
+    let list = [0, 1];
+    for(let i = list.length; i <= num; i++) {
+        list[i] = list[i-2] + list[i-1];
+    }
+    return list[num];
 };
 
 // Do not edit below this line
